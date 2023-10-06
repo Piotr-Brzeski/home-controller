faa9febdbb08e9daf62603934c48c81091053910
Piotr Brzeski <piotr@brzeski.net>
2023-10-06 13:01:32 +0200

    More logs

--- home-controller/home-controller.cpp
+++ home-controller/home-controller.cpp
@@ -13,28 +13,30 @@
 int main(int argc, const char * argv[]) {
 	try {
 		if(argc < 2) {
-			throw std::runtime_error("No configuration path.");
+			throw std::runtime_error("No configuration path");
 		}
 		while(true) {
-			constexpr auto timeout = std::chrono::seconds(5);
+			log::log("Starting home controller");
 			auto start_time = std::chrono::steady_clock::now();
 			auto controller = home::controller(argv[1]);
 			controller.start();
 			controller.wait();
 			auto end_time = std::chrono::steady_clock::now();
 			auto duration = end_time - start_time;
+			auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
+			log::log("Controller finished in " + std::to_string(seconds) + " seconds");
+			constexpr auto timeout = std::chrono::seconds(5);
 			if(duration < timeout) {
-				auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
-				throw std::runtime_error("Controller finished in " + std::to_string(seconds) + " seconds");
+				throw std::runtime_error("Controller could not start");
 			}
 		}
 		return 0;
 	}
 	catch(std::exception &e) {
-		log::log(std::string("Home controlled finished with excetion: ") + e.what());
+		log::log(std::string("Home controller finished with exception: ") + e.what());
 	}
 	catch(...) {
-		log::log("Home controlled finished with unknown excetion");
+		log::log("Home controller finished with unknown exception");
 	}
 	return 1;
 }
